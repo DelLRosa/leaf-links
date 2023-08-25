c53002ff46b5153e6ea174070cc80a685c2373f2
Delaney Green <delaney.rosa1@gmail.com>
2023-08-25 14:51:55 -0500

    Connected MySQl db, configured basic User class to generate a table and added an index page to localhost8080

---
+++ src/main/java/com/greenhouse/leaflinks/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.greenhouse.leaflinks.repositories;
+
+import com.greenhouse.leaflinks.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
