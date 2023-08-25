f82c4ba92b995711c167f3ca4410eb53d9aed7f5
Delaney Green <delaney.rosa1@gmail.com>
2023-08-25 11:40:52 -0500

    first commit

---
+++ src/main/java/com/greenhouse/leaflinks/LeafLinksApplication.java
@@ -0,0 +1,13 @@
+package com.greenhouse.leaflinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafLinksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenhouse/leaflinks/LeafLinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenhouse.leaflinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafLinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
