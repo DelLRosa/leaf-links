431c3046409345f73053782722a2ff65c7ba6ca2
Delaney Green <delaney.rosa1@gmail.com>
2023-09-10 16:42:26 -0500

    added hash function to gradle dependencies and updated User model to store/hash pw.

--- src/main/java/com/greenhouse/leaflinks/models/User.java
+++ src/main/java/com/greenhouse/leaflinks/models/User.java
@@ -1,6 +1,9 @@
 package com.greenhouse.leaflinks.models;
 
 
+import com.sun.istack.NotNull;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -11,6 +14,28 @@ public class User {
     @Id
     @GeneratedValue
     private int id;
+
+    @NotNull
     private String userName;
+
+    @NotNull
+    private String pwHash;
+
+    public User() {}
+
+    public User (String userName, String password) {
+        this.userName=userName;
+        this.pwHash= encoder.encode(password);
+    }
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public String getUserName () {
+        return userName;
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+    }
 
 }
