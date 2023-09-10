692881a90054bfd3760bc1d93f3d5d075316e506
Delaney Green <delaney.rosa1@gmail.com>
2023-09-10 17:08:57 -0500

    Working on user controller classes.

--- src/main/java/com/greenhouse/leaflinks/controllers/UserController.java
+++ src/main/java/com/greenhouse/leaflinks/controllers/UserController.java
@@ -1,4 +1,18 @@
 package com.greenhouse.leaflinks.controllers;
 
+import com.greenhouse.leaflinks.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 }
